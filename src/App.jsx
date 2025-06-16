cd60318fe2c219a41dad27a2c709f67d119876b9
devkoyani <devkoyani521@gmail.com>
2025-06-16 12:14:29 +0530

    create react app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-blue-900 text-red-500 text-2xl p-4">
+      Tailwind CSS is working!
+    </div>
+  )
+}
+
+export default App
